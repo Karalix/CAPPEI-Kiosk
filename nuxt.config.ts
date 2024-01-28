7d3b7c0b8209b3f0a8cb70450d9b9609fef9d75d
Alix Ducros <alix@krlx.fr>
2024-01-28 15:25:16 +0100

    alleezzz

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,44 @@ export default defineNuxtConfig({
     '@vite-pwa/nuxt'
   ],
   pwa: {
-    /* PWA options */
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'CAPPEI-Kiosk',
+      short_name: 'CAPPEI-Kiosk',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico,jpg,jpeg,png}'],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
   }
 })
